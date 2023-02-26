7e0c80f0d961b38000b49cc2f8c34c7def5db88a
neelkantha07 <125341103+neelkantha07@users.noreply.github.com>
2023-02-26 19:23:21 +0530

    Create print_varibales.py

---
+++ print_varibales.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 10:00:14 2023
+
+@author: Programmer
+"""
+"""
+a= " There are Seven Rishis "
+print(a)
+
+#this is for user input
+user_input=input("Please enter your input: ")
+
+#this is for printing the user input
+print("User Input is:",user_input)
+"""
+"""
+a=input("enter a number")
+
+print("%f is the entered number" %float(a))
+"""
+# this is first line
+print("*"*4)
+# 2nd line
+print("*"+(" "*2)+"*")
+# 3 rd line
+print("*"+(" "*2)+"*")
+# 4th line
+print("*"*4)
+
+
+
+
+
+
+
+
+
+
+
+
+
