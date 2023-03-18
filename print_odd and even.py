750f937ec966793091d18d62b1ad134ca8bba39f
neelkantha07 <125341103+neelkantha07@users.noreply.github.com>
2023-03-18 13:00:49 +0530

    Create print_odd and even.py

---
+++ print_odd and even.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 12:26:24 2023
+
+@author: Programmer
+"""
+#Even
+n=1
+while(n<=10):
+    if(n%2==0):
+        print(n,end=" ")
+    n=n+1
+#Odd
+print("\n"+ "*"*10)
+n=1
+while(n<=10):
+    if(n%2==1):
+        print(n,end=" ")
+    n=n+1
